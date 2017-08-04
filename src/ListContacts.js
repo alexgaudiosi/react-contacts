112c2c8175276d66e164376d3e4e739786e732d1
Alex Gaudiosi <alex.gaudiosi@simplybusiness.co.uk>
2017-08-04 11:53:30 +0100

    add create and react router

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -49,7 +49,7 @@ class ListContacts extends Component {
           />
         </div>
 
-        {showingContacts.length != contacts.length && (
+        {showingContacts.length !== contacts.length && (
           <div className="showing-contacts">
             <span>Now showing {showingContacts.length} of {contacts.length}</span>
             <button onClick={this.clearQuery}>Show All</button>
